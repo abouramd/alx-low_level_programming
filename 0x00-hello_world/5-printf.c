73f464e462ef3f6de89276c17abe65cda1de3b34
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-15 18:36:55 -0700

    LOW LEVEL

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Start point
+ *
+ * print with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
