6917a1f28908ec1f9606dbb65ab2ef22478e1bf2
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-15 19:21:52 -0700

    LOW LEVEL

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, s, sizeof(s));
 	return (1);
