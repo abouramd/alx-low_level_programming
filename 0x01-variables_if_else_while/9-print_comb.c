dae5bb0bdac45d3500b65ebef6e9609b69a77cd6
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-16 18:57:28 -0700

    task 6

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	while (c <= '9')
 	{
 		putchar(c++);
-		putchar(',');
-		putchar(' ');
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
