270e25ef944c1d3915241a3b35e61c869cf8a4fc
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-20 16:23:35 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,13 @@
 #include "main.c"
 
-
+/**
+ * _islower - check if the char is lower or not
+ *
+ * @c: the character passed to the function
+ *
+ * Return: 0 if c is not lower and 1 if c is lower
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
