adda8313a10956a46c4e560f42f19bf0371f9807
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-20 16:07:30 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: it is void
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		_putchar(c++);
+}
