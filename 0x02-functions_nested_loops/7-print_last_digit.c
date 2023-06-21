657422273679519902fc5a52fc68dd99a67a73b0
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-20 19:46:28 -0700

    l

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		_putchar((n % 10 ) * -1 + '0');
+		_putchar((n % 10) * -1 + '0');
 		return ((n % 10) * -1);
 	}
-	_putchar((n % 10 ) * 1 + '0');
+	_putchar((n % 10) * 1 + '0');
 	return (n % 10);
 }
