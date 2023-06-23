d5139e3f0482805f32bd584ac5b118617e951f0b
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-22 19:56:54 -0700

    LOW LEVEL

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,12 +10,13 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 400 == 0 && year % 100 == 0) || year % 4 == 0)
 	{
-		if (month > 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -32,4 +33,3 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
-
