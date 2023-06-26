9084c3b8602d1031c7e6636fa6deb92e6be7b7a0
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-26 08:20:56 -0700

    LOW LEVEL

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points
+ * @n: pointer that we will change it value
+ * Return: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
