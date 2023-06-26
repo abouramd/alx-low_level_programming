4103e0dbb027d31312726382a8839e365c49b4ca
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-26 09:45:10 -0700

    LOW LEVEL

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,14 @@ int _atoi(char *s)
 	unsigned int n = 0;
 	int sign = 1;
 
-	while (*s == ' ' || *s == '-' || *s == '+')
+	while (*s)
 	{
 		if (*s == '-')
 			sign *= -1;
+		if (*s >= '0' && *s <= '9')
+			n = n * 10 + *s - 48;
 		s++;
 	}
-	while (*s >= '0' && *s <= '9')
-	{
-		n = n * 10 + *s - 48;
-		s++;
-	}
+	
 	return (n * sign);
 }
