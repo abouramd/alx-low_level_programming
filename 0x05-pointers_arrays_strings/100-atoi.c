21d268f4dcf9bc7581b4d11bb9372ba526b8c3b3
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-26 09:37:41 -0700

    LOW LEVEL

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s : string
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int sign = 1;
+
+	while (*s == ' ')
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		n = n * 10 + *s + 48;
+		s++;
+	}
+	return (n * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+int _atoi(char *s);
 
 #endif
