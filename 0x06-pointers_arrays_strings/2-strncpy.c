9fa9b2ddde9945d28c6b15d3f0877ad21806940c
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-29 13:58:53 -0700

    LOW LEVEL

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,17 +10,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i1;
-	int i2;
+	size_t	index;
 
-	i1 = 0;
-	i2 = 0;
-	dest[n] = 0;
-	while (dest <= src && src[i2] && i2 < n)
-		dest[i1++] = src[i2++];
-	while (src[i1] && i1 < n)
-		i1++;
-	while (dest > src && i1--)
-		dest[i1] = src[i1];
+	index = 0;
+	while (src[index] && index + 1 < n)
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
 	return (dest);
 }
