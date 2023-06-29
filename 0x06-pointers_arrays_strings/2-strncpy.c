57c8458c9ecc826345386d483d121aa98375cb3c
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-29 13:31:23 -0700

    LOW LEVEL

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy two strings
+ * @dest:string of pointer
+ * @src:string of pointer
+ * @n: number of byet to use from src
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i1;
+	int i2;
+
+	i1 = 0;
+	i2 = 0;
+	while (src[i2] && i2 < n)
+		dest[i1++] = src[i2++];
+	dest[i1] = 0;
+	return (dest);
+}
