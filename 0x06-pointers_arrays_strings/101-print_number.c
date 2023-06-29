f33dc0ce524cb69c22006976e972938f0dad0192
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-29 16:02:04 -0700

    LOW LEVEL

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,15 +10,15 @@
 
 void print_number(int n)
 {
+	unsigned int o;
+	
+	o = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		if (n <= -10)
-			print_number((n / 10) * -1);
-		print_number((n % 10) * -1);
-		return;
+		o = n * -1;
 	}	
-	if (n >= 10)
-		print_number(n / 10);
-	_putchar(n % 10 + 48);
+	if (o >= 10)
+		print_number(o / 10);
+	_putchar(o % 10 + 48);
 }
