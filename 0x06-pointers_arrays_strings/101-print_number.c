a90c47812f4c2884695f28ff71dcc0f6c6704e88
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-06-29 15:41:03 -0700

    LOW LEVEL

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: number
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		print_number(n / 10 * -1);
+		print_number(n % 10 * -1);
+		return;
+	}	
+	if (n >= 10)
+		print_number(n / 10);
+	_putchar(n % 10 + 48);
+}
