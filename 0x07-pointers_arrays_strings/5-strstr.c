859e012a5bf82caa342689c11e7cb6303075c0c9
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-03 07:54:28 -0700

    LOW LEVEL

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	while (haystack[i])
+	while (haystack && needle && haystack[i])
 	{
 		j = 0;
 		while (haystack[i + j] == needle[j])
