e477362e45103adb918828724eab133487922ad0
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-05 01:28:44 -0700

    LOW LEVEL

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string in reverse.
+ * @str: the string pointer
+ * Return: no return value
+ */
+void _print_rev_recursion(char *str)
+{
+	if (str && *str)
+	{
+		_puts_recursion(str + 1);
+		_putchar(*str);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
-
+void _print_rev_recursion(char *s);
 
 
 
