f193be52bc40610b6ff584b0a0a038b81b0b280f
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-05 15:36:06 -0700

    LOW LEVEL

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _ip - recursion to find palindrome
+ * @n: string
+ * @i: checker
+ * @j: checker
+ * Return: 1 if is palindrome or 0 if not
+ */
+
+int _ip(char* s, int i, int j)
+{
+	if (i >= j)
+		return (1);
+	if (s[i] != s[j])
+		return (0);
+	return (_ip(s, ++i, --j));
+}
+
+/**
+ * is_palindrome - palindrome
+ * @n: string
+ * Return: 1 if is prime or 0 if not
+ */
+
+int is_palindrome(char* n)
+{
+	int i = 0;
+
+	while (n[i])
+		i++;
+	return (_ip(n, 0, --i));
+}
