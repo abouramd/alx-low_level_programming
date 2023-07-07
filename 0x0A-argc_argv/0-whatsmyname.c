8483fbeb82ae36e94ffbdfd0718c9dc3b9a9e6c6
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-07 15:18:08 -0700

    LOW LEVEL

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+
 int main(int ac, char **av)
 {
 	int i = 0;
 
-	while ((*av)[i])
-		_putchar((*av)[i++]);
+	while (av[0][i])
+		_putchar(av[0][i++]);
 	_putchar('\n');
 	return (0);
 }
