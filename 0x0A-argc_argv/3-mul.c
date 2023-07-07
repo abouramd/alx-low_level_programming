783011809ad44fbe9d6f3d3095fc68978f9befed
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-07 15:37:30 -0700

    LOW LEVEL

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - start of the program
+ *
+ * @ac: argc
+ *
+ * @av: argv
+ *
+ * Return: 0, 1 if argc diff 3
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+		return (1);
+	printf("%d\n", atoi(av[1]) * atoi(av[2]));
+	return (0);
+}
