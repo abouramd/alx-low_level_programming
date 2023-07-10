f1a1381961b120c559dca157501551d91ad3eda4
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-10 13:58:25 -0700

    LOW LEVEL

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * alloc_grid - alloc a grid of integers
+ * free_grid - alloc a grid of integers
  * @grid: the address of the two dimensional grid
  * @height: height of the grid
  * Return: grid of integer.
@@ -8,13 +8,13 @@
 
 void free_grid(int **grid, int height)
 {
-    int h;
+	int h;
 
-    h = 0;
-    while (h < height)
-    {
+	h = 0;
+	while (h < height)
+	{
 		free(grid[h]);
-        h++;
-    }
+		h++;
+	}
 	free(grid);
 }
