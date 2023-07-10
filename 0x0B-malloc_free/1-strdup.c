252ffde77591714ea99d4568316eb16b74b96a40
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-10 11:50:02 -0700

    LOW LEVEL

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - copy of the string
+ * @str: string
+ * Return: returns a pointer to a new string which is a duplicate of the string str
+ */
+
+char *_strdup(char *str)
+{
+	char *d;
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	d = malloc(i + 1);
+	if (!d)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		d[i] = str[i];
+		i++;
+	}
+	return (d);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 # include <stdlib.h>
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 #endif
 
