5304cef8f5446da7019c2a7d12c70365bdbce673
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-10 13:27:56 -0700

    LOW LEVEL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,24 +6,35 @@
  * Return: returns a pointer to a new string which is a duplicate of the string str
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
 	char *d;
 	unsigned int i = 0;
+	unsigned int j = 0;
 
-	if (str == NULL)
-		return (NULL);
-	while (str[i])
+	if (s1 == NULL || s2 == NULL)
+	{
+		d = malloc(1);
+		if (d == NULL)
+			return (NULL);
+		*d = 0;
+		return (d);
+	}
+	while (s1[i])
+		i++;
+	while (s2[j++])
 		i++;
 	d = malloc(i + 1);
 	if (!d)
 		return (NULL);
 	i = 0;
-	while (str[i])
+	while (s1[i])
 	{
-		d[i] = str[i];
+		d[i] = s1[i];
 		i++;
 	}
+	while (s2[j])
+		d[i++] = s2[j++];
 	d[i] = 0;
 	return (d);
 }
