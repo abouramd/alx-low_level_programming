8f9f0a3c69d11f467a34692708b31d58ac5bb2e4
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-10 13:30:30 -0700

    LOW LEVEL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,29 +12,21 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		d = malloc(1);
-		if (d == NULL)
-			return (NULL);
-		*d = 0;
-		return (d);
-	}
-	while (s1[i])
+	while (s1 && s1[i])
 		i++;
-	while (s2[j++])
+	while (s2 && s2[j++])
 		i++;
 	d = malloc(i + 1);
 	if (!d)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		d[i] = s1[i];
 		i++;
 	}
 	j = 0;
-	while (s2[j])
+	while (s2 && s2[j])
 		d[i++] = s2[j++];
 	d[i] = 0;
 	return (d);
