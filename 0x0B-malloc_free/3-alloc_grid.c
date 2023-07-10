3ec756666381ecdd13bb58e5827602e41b9a6e81
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-10 14:45:03 -0700

    LOW LEVEL

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,37 +5,37 @@
  * @height: height of the grid
  *
  * Return: grid of integer.
- */ 
+ */
 
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
 	int w;
-    int h;
+	int h;
 
-    h = 0;
+	h = 0;
 	if (width == 0 && height == 0)
 		return (NULL);
-	ptr = malloc (sizeof(int *) * height);
+	ptr = malloc(sizeof(int *) * height);
 	if (!ptr)
 		return (NULL);
-    while (h < height)
-    {
-		ptr[h] = malloc (sizeof(int) * width);
-        if (!ptr[h])
+	while (h < height)
+	{
+		ptr[h] = malloc(sizeof(int) * width);
+		if (!ptr[h])
 		{
 			while (h--)
 				free(ptr[h]);
-			free (ptr);
+			free(ptr);
 			return (NULL);
 		}
 		w = 0;
-        while (w < width)
-        {
+		while (w < width)
+		{
 			ptr[h][w] = 0;
-        	w++;
-        }
-        h++;
-    }
+			w++;
+		}
+		h++;
+	}
 	return (ptr);
 }
