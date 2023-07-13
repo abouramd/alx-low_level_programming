bb4da79ea02c666a06b65694b17607a41688f096
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-13 15:08:25 -0700

    LOW LEVEL

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,9 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i = 0;
+	char *p;
+
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0)
@@ -21,13 +24,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	unsigned int i = 0;
-	char *p;
 
 	p = malloc(new_size);
 	if (!p)
 		return (NULL);
-	while (i < new_size && i < old_size)
+	while ((unsigned int)i < new_size && (unsigned int)i < old_size)
 	{
 		p[i] = ((char *)ptr)[i];
 		i++;
