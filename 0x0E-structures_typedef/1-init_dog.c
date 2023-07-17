12ca06c0415e5b9569d30bfb181f2a6b53289ce7
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-17 11:34:48 -0700

    LOW LEVEL

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * int dog - init structer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
