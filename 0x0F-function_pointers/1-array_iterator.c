54d2b08fed5023b147f168eb7f8e61ed1ea4683f
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-19 14:24:30 -0700

    LOW LEVEL

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @array: pointe to an array
+ * @size: the size of the array
+ * @action: function
+ * Return: void
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
