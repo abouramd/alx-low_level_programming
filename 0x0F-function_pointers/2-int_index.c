e2e45445762fe6ef11582b7617c30d27278c8cda
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-19 14:38:31 -0700

    LOW LEVEL

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - array_iterator
+ * @array: pointe to an array
+ * @size: the size of the array
+ * @cmp: function
+ * Return: void
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i = 0;
+
+	while (cmp && array && i < size)
+	{
+		if (!cmp(array[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
