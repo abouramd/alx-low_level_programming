005fc6fdea4192633cb2e471bffbd98efa373068
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-19 15:38:02 -0700

    LOW LEVEL

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,10 +10,23 @@
 
 int main(int ac, char **av)
 {
-	if (ac != 4 || !get_op_func(av[2]))
+	if (ac != 4)
 	{
 		printf("Error\n");
 		return (98);
+	}
+	int a = atoi(av[1]);
+	int b = atoi(av[3]);
+
+	if (b == 0 && (av[2] == '/' || av[2] == '%'))
+	{
+		printf("Error\n");
+		return (100);
+	}
+	if (!get_op_func(av[2]))
+	{
+		printf("Error\n");
+		return (99);
 	}
 	printf("%d\n", (int)((get_op_func(av[2]))(atoi(av[1]), atoi(av[3]))));
 	return (0);
