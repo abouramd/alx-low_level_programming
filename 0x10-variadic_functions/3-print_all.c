4512922a1a2db1a39c44ec8b6424f9f8e767885e
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-20 14:05:28 -0700

    LOW LEVEL

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print all its parameters
+ * @f: format
+ * Return: void
+ */
+
+void print_all(const char *f, ...)
+{
+	va_list p;
+	unsigned int index = 0;
+
+	va_start(p, f);
+	while (f && f[index])
+	{
+		if (f[index] == 'c' && ++index)
+		{
+			printf("%c", va_arg(p, int));
+			continue;
+		}
+		if (f[index] == 'd' && ++index)
+		{
+			printf("%d", va_arg(p, int));
+			continue;
+		}
+		while (f[index] == 'f' && ++index)
+		{
+			printf("%f", va_arg(p, double));
+			continue;
+		}
+		char *s = va_arg(p, char *);
+
+		if (s)
+			printf("%s", s);
+		else
+			printf("(nil)");
+		index++;
+	}
+	va_end(p);
+	printf("\n");
+}
