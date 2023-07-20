eb936a1612d197345ddc05d94e31c0b7cadc4b27
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-20 13:37:13 -0700

    LOW LEVEL

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all its parameters
+ * @separator: separator
+ * @n: number of param
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int index = 0;
+
+	va_start(p, n);
+	while (index++ < n)
+	{
+		printf("%s", va_arg(p, char *));
+		if (index != n && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(p);
+}
