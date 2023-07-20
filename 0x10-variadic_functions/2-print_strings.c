2c88e9515c06a35966d53c0e1a5fe3e9e6f2422d
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-20 13:51:17 -0700

    LOW LEVEL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (index != n && separator)
 			printf("%s", separator);
 	}
-	printf("\n");
 	va_end(p);
+	printf("\n");
 }
