e942e870ba534826a16ab8a2649742b660ee0d83
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 23:16:38 +0100

    @abouramd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-	hello db 'Hello, Holberton\n', 0   ; Null-terminated string with newline character (0xA) at the end
+	hello db 'Hello, Holberton', 0   ; Null-terminated string with newline character (0xA) at the end
 
 section .text
 	global main
