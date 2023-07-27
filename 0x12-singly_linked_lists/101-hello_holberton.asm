095b24d634385c44ad7ceaf3495150057d29fa34
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 23:12:18 +0100

    @abouramd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,3 @@
-section .text
-    hello: db "hello\n", 6
-    hellolen: equ $-hello
-
-section .text
-    global _start
-
-_start:
-    mov rax,1
-    mov rdi,1
-    mov rsi,hello
-    mov rdx,hellolen
-    syscall
-
-    mov rax,60
-    mov rdi,0
-    syscall
-
-
 section .data
     hello db 'Hello, Holberton', 0
 
