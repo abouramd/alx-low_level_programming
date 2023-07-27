e946f1ef4dad69e21d30273f56719d5290ff4111
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 22:02:47 +0100

    @abouramd

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - add node
+ * @h: head of the list
+ * Return: the new head
+ */
+
+list_t *add_node(list_t **h, const char *str)
+{
+	size_t i = 0;
+    list_t *new = NULL;
+
+    if (h)
+    {
+        new = malloc(sizeof(list_t));
+        if (!new)
+            return (NULL);
+        new->str = strdup(str);
+        if (!new->str)
+        {
+            free(new);
+            return (NULL);
+        }
+        while (str[i])
+            i++;
+        new->len = i;
+        new->next = *h;
+        *h = new;
+	}
+	return (new);
+}
