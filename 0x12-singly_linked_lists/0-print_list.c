3b81041ca934f6a8167f963436a6fe5f1e338a51
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-07-27 07:26:27 -0700

    @abouramd

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - print node content
+ * @h: struct list_t
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] (%s)\n", h->len, h->str);
+		i++;
+	}
+	return (i);
+}
