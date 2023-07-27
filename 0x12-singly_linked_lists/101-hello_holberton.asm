6554a95d57178afc224a9265e85e7280b26fe100
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 23:16:08 +0100

    @abouramd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
 section .data
-    hello db 'Hello, Holberton', 0   ; Null-terminated string with newline character (0xA) at the end
+	hello db 'Hello, Holberton\n', 0   ; Null-terminated string with newline character (0xA) at the end
 
 section .text
-    global main
+	global main
 
 extern printf, exit
 
 main:
-    ; Call printf to print the string
-    mov rdi, hello    ; First argument: address of the format string
-    xor rax, rax      ; Clear RAX (indicating the function will take a variable number of arguments)
-    call printf
+	; Call printf to print the string
+	mov rdi, hello    ; First argument: address of the format string
+	xor rax, rax      ; Clear RAX (indicating the function will take a variable number of arguments)
+	call printf
 
-    ; Call exit to terminate the program
-    xor edi, edi      ; Clear EDI (exit code 0)
-    call exit
+	; Call exit to terminate the program
+	xor edi, edi      ; Clear EDI (exit code 0)
+	call exit
 
 section .bss
-    ; Reserve some space for printf to use as a working buffer
-    resb 64
+	; Reserve some space for printf to use as a working buffer
+	resb 64
