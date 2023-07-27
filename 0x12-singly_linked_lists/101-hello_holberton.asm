edc4aa0436dfbad71dbb8941d1f2f3390fe4e28d
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 23:10:38 +0100

    @abouramd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,15 +5,15 @@ section .text
 section .text
     global _start
 
-    _start:
-        mov rax,1
-        mov rdi,1
-        mov rsi,hello
-        mov rdx,hellolen
-        syscall
+_start:
+    mov rax,1
+    mov rdi,1
+    mov rsi,hello
+    mov rdx,hellolen
+    syscall
 
-        mov rax,60
-        mov rdi,0
-        syscall
-
+    mov rax,60
+    mov rdi,0
+    syscall
+    
 
