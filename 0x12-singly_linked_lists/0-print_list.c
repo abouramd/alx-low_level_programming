44b33a6f57da08dac4a5445eb915bd13eeeedf42
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 22:07:01 +0100

    @abouramd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] (%s)\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
