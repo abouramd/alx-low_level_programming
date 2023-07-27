13345e3bddccf0ba53ab9a4e5a9a77d8c2ae3fe4
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 22:36:17 +0100

    @abouramd

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - free a linked list
+ * @head: struct list_t
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	if (h)
+	{
+		free_list(head->next);
+		free(head->str);
+		free(head);
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,9 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **h, const char *str);
 list_t *add_node_end(list_t **h, const char *str);
+void free_list(list_t *head);
+
+
+
 
 #endif
