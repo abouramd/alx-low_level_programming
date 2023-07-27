0aaa2f4c94b20d70984e2b637e85ad3208c1a14a
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 22:46:14 +0100

    @abouramd

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * before_main - print before the main function is executed
+ * Return: void
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -10,37 +10,5 @@
  */
 int main(void)
 {
-	list_t *head;
-
-	head = NULL;
-	add_node_end(&head, "Alexandro");
-	add_node_end(&head, "Asaia");
-	add_node_end(&head, "Augustin");
-	add_node_end(&head, "Bennett");
-	add_node_end(&head, "Bilal");
-	add_node_end(&head, "Chandler");
-	add_node_end(&head, "Damian");
-	add_node_end(&head, "Daniel");
-	add_node_end(&head, "Dora");
-	add_node_end(&head, "Electra");
-	add_node_end(&head, "Gloria");
-	add_node_end(&head, "Joe");
-	add_node_end(&head, "John");
-	add_node_end(&head, "John");
-	add_node_end(&head, "Josquin");
-	add_node_end(&head, "Kris");
-	add_node_end(&head, "Marine");
-	add_node_end(&head, "Mason");
-	add_node_end(&head, "Praylin");
-	add_node_end(&head, "Rick");
-	add_node_end(&head, "Rick");
-	add_node_end(&head, "Rona");
-	add_node_end(&head, "Siphan");
-	add_node_end(&head, "Sravanthi");
-	add_node_end(&head, "Steven");
-	add_node_end(&head, "Tasneem");
-	add_node_end(&head, "William");
-	add_node_end(&head, "Zee");
-	print_list(head);
 	return (0);
 }
