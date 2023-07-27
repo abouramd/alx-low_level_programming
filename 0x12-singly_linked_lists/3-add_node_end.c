b20b19725ef2353df5e71c6a9e7a6b99024b16ef
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 22:14:31 +0100

    @abouramd

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add node
+ * @h: head of the list
+ * @str: the content
+ * Return: the new head
+ */
+
+list_t *add_node_end(list_t **h, const char *str)
+{
+	size_t i = 0;
+	list_t *new = NULL;
+	list_t *tmp = NULL;
+
+	if (h)
+        return (NULL);
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (str[i])
+		i++;
+	new->next = NULL;
+	new->len = i;
+    if (*h)
+        *h = new;
+    else
+    {
+        tmp = *h;
+        while (tmp->next)
+            tmp = tmp->next;
+	    tmp->next = new;
+    }
+	return (*h);
+}
