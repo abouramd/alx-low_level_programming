3826ecbac56a9bdf3208d8df46bf8a199b81fe91
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 23:12:10 +0100

    @abouramd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -15,5 +15,23 @@ _start:
     mov rax,60
     mov rdi,0
     syscall
-    
 
+
+section .data
+    hello db 'Hello, Holberton', 0
+
+section .text
+    global _start
+
+extern printf, exit
+
+_start:
+    mov rdi, hello
+    xor rax, rax
+    call printf
+
+    xor edi, edi
+    call exit
+
+section .bss
+    resb 64
