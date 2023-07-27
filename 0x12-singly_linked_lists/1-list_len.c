8233c8ca369dd74afbcc39b51510134b9df740dd
Abdelhay Bouramdane <abouramd@e1r12p8.1337.ma>
2023-07-27 21:53:32 +0100

    @abouramd

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - num node
+ * @h: struct list_t
+ * Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
