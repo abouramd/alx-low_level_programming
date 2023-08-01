c08cae29eaa6ce9570d963edf34c79f53aaaa3c5
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-08-01 04:00:23 -0700

    @abouramd

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - find a node in a linked listint
+ * @head: struct listint_t
+ * @index: index of the target
+ * Return: return the addr of the node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head -> next;
+		i++;
+	}
+	return (NULL);
+}
