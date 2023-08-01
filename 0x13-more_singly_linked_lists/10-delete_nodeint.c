e8d6a3e18675c35b51813dffb238ad3cfefa3b1c
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-08-01 05:22:25 -0700

    @abouramd

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,16 +1,37 @@
 #include "lists.h"
 
 /**
- * free_listint - free a linked listint
+ * insert_nodeint_at_index - insert nodeint at index
  * @head: struct listint_t
- * Return: void
+ * @idx: index of the node
+ * @n: the data of the new node
+ * Return: the addr of new node
  */
 
-void free_listint(listint_t *head)
+int delete_nodeint_at_index(listint_t **head, unsigned int idx)
 {
-	if (head)
+	listint_t *tmp = NULL;
+	listint_t *buff = NULL;
+
+	if (!head)
+		return (-1);
+	tmp = *head;
+	if (*head && idx == 0)
 	{
-		free_listint(head->next);
-		free(head);
+		(*head) = tmp->next;
+		free(tmp);
+		return (1);
 	}
+	while (tmp && tmp->next)
+	{
+		if (--idx == 0)
+		{
+			buff = tmp->next;
+			tmp->next = buff->next;
+			free(buff);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (-1);
 }
