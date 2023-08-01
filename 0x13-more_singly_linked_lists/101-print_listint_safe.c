353d4e5e34965c5f99393187f5f750c2f11ad5d7
abouramd <abouramd@student.1337.ma>
2023-08-01 22:26:09 +0100

    LOW LEVEL

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print a linked list safe
+ * @head: the head of the list
+ * Return: the size of the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+
+	while (head)
+	{
+		printf("[%p] %d\n", head, head->n);
+		head = head->next;
+		count++;
+	}
+	return (count);
+}
