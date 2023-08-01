50950c09d97f52e6dbd3c6b5828b3baa2f111760
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-08-01 04:01:40 -0700

    @abouramd

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (i == index)
 			return (head);
-		head = head -> next;
+		head = head->next;
 		i++;
 	}
 	return (NULL);
