0015826473ca982a62c73ac84b7f48ce25512ddb
abdelhay bouramdane <bouramadane2002@gmail.com>
2023-08-01 05:15:42 -0700

    @abouramd

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,8 +22,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (!*head)
 			return (NULL);
 		(*head)->next = tmp;
-		tmp->n = n;
-		return (tmp);
+		(*head)->n = n;
+		return (*head);
 	}
 	while (tmp)
 	{
