42c81365b514b6329854aec837e0728adeb03936
abouramd <abouramd@student.1337.ma>
2023-08-01 22:52:02 +0100

    LOW LEVEL

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,21 @@
 #include "lists.h"
+#include <stddef.h>
+
+int checkPrev(listint_t *h, listint_t *node, size_t count)
+{
+	size_t i = 0;
+
+	while (h && i < count)
+	{
+		if (h == node)
+		{
+			printf("-> [%p] %d\n", (void *)h, h->n);
+			return (1);
+		}
+		i++;
+	}
+	return (0); 
+}
 
 /**
  * print_listint_safe - print a linked list safe
@@ -9,9 +26,13 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
+	listint_t *tmp;
 
+	tmp = head;
 	while (head)
 	{
+		if (checkPrev(tmp, head, count))
+			return (98);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 		count++;
