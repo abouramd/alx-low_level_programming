971b5de8246c0c23d7f07512dee75709ae600ff9
Abdelhay Bouramdane <abouramd@e1r5p3.1337.ma>
2023-08-07 21:21:20 +0100

    @abouramd

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - write text file
+ * @filename: file name
+ * @text_content: content of the file
+ * Return: -1 if an error or 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t i;
+	int fd;
+
+	if (!filename)
+		return (-1);
+	i = 0;
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,26 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+int create_file(const char *filename, char *text_content);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #endif
