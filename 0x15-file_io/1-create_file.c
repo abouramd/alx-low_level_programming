da9e17fcf1ce055847e67d7c36aaccbd86c0f762
Abdelhay Bouramdane <abouramd@e1r5p3.1337.ma>
2023-08-07 21:24:15 +0100

    @abouramd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,10 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t i;
 	int fd;
 
 	if (!filename)
 		return (-1);
-	i = 0;
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd < 0)
 		return (-1);
